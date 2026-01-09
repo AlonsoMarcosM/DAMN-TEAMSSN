94903b7f04386b0b96780bc0c37d5eb194452f23
MarioPG21 <mario.panos2@alu.uclm.es>
2026-01-09 18:55:58 +0100

    Creado script de configuración rápida

---
+++ scripts/config_rapida.ps1
@@ -0,0 +1,64 @@
+# Definir rutas
+$RepoRoot = Resolve-Path "$PSScriptRoot\.."
+$EnvsPath = Join-Path $RepoRoot "envs"
+$InfraPath = Join-Path $RepoRoot "infra"
+
+# Verificar credenciales de AWS
+Write-Host "Verificando conexión con AWS..." -ForegroundColor Cyan
+aws sts get-caller-identity > $null 2>&1
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Iniciando configuración de credenciales..." -ForegroundColor Yellow
+    aws configure
+    aws sts get-caller-identity > $null 2>&1
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Error: Las credenciales siguen fallando." -ForegroundColor Red
+        exit 1
+    }
+}
+Write-Host "AWS Conectado." -ForegroundColor Green
+
+# Datos del usuario
+$Suffix = Read-Host "Escribe el sufijo con el que se crearán los archivos (por ejemplo, las iniciales de tu nombre)"
+if ($Suffix -eq "") { Write-Error "El sufijo es obligatorio"; exit 1 }
+
+$Email = Read-Host "Escribe el email al que quieres que lleguen los correos de aviso"
+if ($Email -eq "") { Write-Error "El email es obligatorio"; exit 1 }
+
+$Account = aws sts get-caller-identity --query "Account" --output text
+$ProfileName = "LabInstanceProfile"
+$RoleArn = "arn:aws:iam::" + $Account + ":role/LabRole"
+
+# Generar archivo de configuración
+$RutaArchivo = Join-Path $EnvsPath "$Suffix.auto.tfvars"
+
+# Borramos archivo viejo
+if (Test-Path $RutaArchivo) { Remove-Item $RutaArchivo -Force }
+
+Add-Content -Path $RutaArchivo -Value ('resource_suffix = "' + $Suffix + '"')
+Add-Content -Path $RutaArchivo -Value ('admin_email = "' + $Email + '"')
+Add-Content -Path $RutaArchivo -Value 'aws_region = "us-east-1"'
+Add-Content -Path $RutaArchivo -Value 'ami_id = ""'
+Add-Content -Path $RutaArchivo -Value 'enable_ssm = true'
+Add-Content -Path $RutaArchivo -Value ('existing_instance_profile_name = "' + $ProfileName + '"')
+Add-Content -Path $RutaArchivo -Value ('existing_lambda_role_arn = "' + $RoleArn + '"')
+Add-Content -Path $RutaArchivo -Value ('allowed_admin_cidr = "84.121.114.164/32"')
+
+Write-Host "Archivo de variables generado: $RutaArchivo" -ForegroundColor Green
+
+# Terraform
+Write-Host "Iniciando Terraform..." -ForegroundColor Cyan
+
+$ArgChdir   = "-chdir=$InfraPath"
+$ArgVarFile = "-var-file=$RutaArchivo"
+
+& terraform $ArgChdir init -input=false
+
+& terraform $ArgChdir apply $ArgVarFile -auto-approve
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Fallo en el despliegue."
+    exit 1
+}
+
+Wrtite-Host "Ejecutar 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o ConnectTimeout=5 -p 22 fakeuser@<ip-publica-ec2>' para hacer un ataque."
